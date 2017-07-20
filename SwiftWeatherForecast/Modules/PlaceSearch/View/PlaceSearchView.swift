72d011b1a5d618b95529cc7bc567c9d066b54ed7
Lucio <lfonseca@avenuecode.com>
2017-07-20 11:02:53 -0300

    Search Screen basic logic completed.

--- SwiftWeatherForecast/Modules/PlaceSearch/View/PlaceSearchView.swift
+++ SwiftWeatherForecast/Modules/PlaceSearch/View/PlaceSearchView.swift
@@ -5,7 +5,47 @@
 
 import Foundation
 import UIKit
+import NVActivityIndicatorView
 
-class PlaceSearchView: UIViewController, PlaceSearchViewProtocol {
+class PlaceSearchView: UIViewController, PlaceSearchViewProtocol, NVActivityIndicatorViewable {
     var presenter: PlaceSearchPresenterProtocol?
+    var dataSource: PlaceSearchDataSource?
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Search Places"
+        self.dataSource = PlaceSearchDataSource(tableView: self.tableView)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.searchBar.becomeFirstResponder()
+    }
+
+    //MARK - Protocol Methdos
+
+    func showPlaces(places: [PlaceSearchItem]) {
+        self.dataSource?.places = places
+    }
+
+    func showProgressIndicator() {
+        self.startAnimating(type: .ballTrianglePath)
+    }
+
+    func removeProgressIndicator() {
+        self.stopAnimating()
+    }
+}
+
+
+//MARK - Search Bar Delegate
+
+extension PlaceSearchView : UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        self.presenter?.searchEvent(searchKey: searchBar.text)
+    }
 }
